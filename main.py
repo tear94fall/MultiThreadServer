ea7a821cec18fda053c67fb12f906aae478f06a3
tear94fall <tear94fall@naver.com>
2019-05-10 19:50:21 +0900

    1.commandExecuter 작동하지 않도록 수정 2.SendAll 메서드 주석 처리함 (모두 클라이언트에게 데이터를 보내는 것으로 최대한 사용자체 할것)

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ andRaspTCP = tcpServer.TCPServer(commandQueue, "127.0.0.1", 8888)
 andRaspTCP.start()
 
 # set module to executer
+'''
 commandExecuter = executer.Executer(andRaspTCP)
 while True:
     try:
@@ -21,7 +22,9 @@ while True:
         commandExecuter.startCommand(command)
     except:
         pass
+'''
 
+# 모든 클라이언트에게 데이터 전송
 # while True:
 #    time.sleep(3)
 #    andRaspTCP.sendAll("321\n")
